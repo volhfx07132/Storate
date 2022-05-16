360ca79a102d122045f0982347b140bb6b9d5e35
Le Hong vo <volhfx07132@funix.edu.vn>
2022-05-16 10:32:03 +0700

    update

--- ERC20/Store.sol
+++ ERC20/Store.sol
@@ -8,12 +8,13 @@ contract Store is ERC20, ERC20Burnable, Ownable{
 
     uint256 private FEE_USE_APP = 10000000000000000; // 0.01 ETH
     uint256 private VALUE_SWAP_TOKEN = 1000; // 1ETH = 1000 token
-    uint256 private SPACE_TIME_CLAIM = 10;
-    uint256 private SPACE_TIME_REFUND = 20;
+    uint256 private SPACE_TIME_CLAIM = 100; // after 100 seconds ==> can claim token
+    uint256 private SPACE_TIME_REFUND = 200; // after 200 seconds ==> can refund token
     address private ADMIN = 0x3Cb06A7709a2511e48a091979b9b68B27999AfaB;
 
     event deposit(address user, uint256 amount_in);
     event claim(address sender, uint256 token_out);
+    event refund(address receiver, uint256 token_out);
 
     struct BuyerInfo{
         uint256 valueInput;
@@ -52,6 +53,12 @@ contract Store is ERC20, ERC20Burnable, Ownable{
         require(listBuyerInfor[msg.sender].currnetTimeClaim < block.timestamp, "NOT TIME TO CLIAM TOKEN");
         transferFrom(ADMIN, msg.sender, listBuyerInfor[msg.sender].valueOutput);
         emit claim(msg.sender, listBuyerInfor[msg.sender].valueOutput);
+    }
+
+    function refundEth() public {
+        require(listBuyerInfor[msg.sender].currentTimeRefund < block.timestamp, "NOT TIME TO CLIAM TOKEN");
+        payable(msg.sender).transfer(listBuyerInfor[msg.sender].valueOutput * (10 ** 18));
+        emit refund(msg.sender, listBuyerInfor[msg.sender].valueOutput * (10 ** 18));
     }
 
     function getBalance(address _address) public view returns(uint256){
